bb3a638cf7a622b495c91333d1d8b74d5924208e
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-09-24 09:20:27 -0400

    added unit testing

---
+++ qc.c
@@ -0,0 +1,103 @@
+#include "qc.h"
+#include <gc.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+void qc_init() {
+	GC_INIT();
+	srand((unsigned int) time(NULL));
+}
+
+void gen_bool(void* data) {
+	bool b = rand() % 2 == 0;
+
+	qc_return(bool, b);
+}
+
+void gen_int(void* data) {
+	int i = rand();
+
+	qc_return(int, i);
+}
+
+void gen_char(void* data) {
+	char c = (char) (rand() % 128);
+
+	qc_return(char, c);
+}
+
+void _gen_array(void* data, fp gen, int len, size_t size) {
+	int i;
+	for (i = 0; i < len; i++) {
+		gen(data + i * size);
+	}
+}
+
+void gen_string(void* data) {
+	int len = rand() % 100;
+
+	size_t size = sizeof(char);
+
+	char* s = (char*) GC_MALLOC(len * size);
+
+	gen_array(s, gen_char, len, size);
+
+	qc_return(char*, s);
+}
+
+void print_bool(void* data) {
+	bool b = (* (bool*) data);
+
+	if (b) {
+		printf("true");
+	}
+	else {
+		printf("false");
+	}
+}
+
+void print_int(void* data) {
+	int i = qc_args(int, 0, sizeof(int));
+
+	printf("%d", i);
+}
+
+void print_char(void* data) {
+	char c = qc_args(char, 0, sizeof(char));
+
+	printf("\'%c\'", c);
+}
+
+void print_string(void* data) {
+	char* s = qc_args(char*, 0, sizeof(char*));
+
+	printf("%s", s);
+}
+
+void _for_all(prop property, int arglen, fp generators[], fp printers[], size_t max_size) {
+	int i, j;
+
+	void* values = GC_MALLOC(arglen * max_size);
+
+	for (i = 0; i < 100; i++) {
+		for (j = 0; j < arglen; j++) {
+			generators[j](values + j * max_size);
+		}
+
+		bool holds = property(values);
+
+		if (!holds) {
+			printf("*** Failed!\n");
+		
+			for (j = 0; j < arglen; j++) {
+				printers[j](values + j * max_size);
+				printf("\n");
+			}
+
+			return;
+		}
+	}
+
+	printf("+++ OK, passed 100 tests.\n");
+}
---
+++ qc.h
@@ -0,0 +1,43 @@
+#ifndef QC
+#define QC
+
+#include <stdlib.h>
+
+typedef int bool;
+
+#ifndef true
+	#define true 1
+#endif
+
+#ifndef false
+	#define false 0
+#endif
+
+typedef void (*fp)(void*);
+typedef bool (*prop)(void*);
+
+void qc_init();
+
+#define qc_return(type, value) ((* (type*) data) = value)
+#define qc_args(type, n, max_size) ((* (type*) (data + n * max_size)))
+
+void gen_bool(void* data);
+void gen_int(void* data);
+void gen_char(void* data);
+
+void _gen_array(void* data, fp gen, int len, size_t size);
+
+#define gen_array(data, gen, len, size) (_gen_array(data, (fp) gen, len, size))
+
+void gen_string(void* data);
+
+void print_bool(void* data);
+void print_int(void* data);
+void print_char(void* data);
+void print_string(void* data);
+
+void _for_all(prop property, int arglen, fp generators[], fp printers[], size_t max_size);
+
+#define for_all(property, arglen, generators, printers, max_size) (_for_all((prop) property, arglen, generators, printers, max_size))
+
+#endif
