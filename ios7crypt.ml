1a3f5718cdb6ae25a6beaa520e7098e742598559
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-10-01 19:48:40 -0400

    added xlat' and xlat

--- ios7crypt.ml
+++ ios7crypt.ml
@@ -20,6 +20,7 @@ Run:
 
 open Getopt
 open QuickCheck
+open List
 
 type mode = Encrypt | Decrypt | Help | Test
 
@@ -27,14 +28,30 @@ let mode = ref Help
 and password = ref ""
 and hash = ref ""
 
-let encrypt : string -> string =
-	fun password -> password (* ... *)
+let xlat' : int list = [
+	0x64; 0x73; 0x66; 0x64; 0x3b; 0x6b; 0x66; 0x6f;
+	0x41; 0x2c; 0x2e; 0x69; 0x79; 0x65; 0x77; 0x72;
+	0x6b; 0x6c; 0x64; 0x4a; 0x4b; 0x44; 0x48; 0x53;
+	0x55; 0x42; 0x73; 0x67; 0x76; 0x63; 0x61; 0x36;
+	0x39; 0x38; 0x33; 0x34; 0x6e; 0x63; 0x78; 0x76;
+	0x39; 0x38; 0x37; 0x33; 0x32; 0x35; 0x34; 0x6b;
+	0x3b; 0x66; 0x67; 0x38; 0x37
+]
 
-let decrypt : string -> string =
-	fun hash -> hash (* ... *)
+let rec xlat i len =
+	if len < 1 then
+		[]
+	else
+		append [nth xlat' (i mod (length xlat'))] (xlat (i + 1) (len - 1))
 
-let prop_reversible : string -> bool =
-	fun password -> decrypt (encrypt password) = password
+let encrypt password =
+	password (* ... *)
+
+let decrypt hash =
+	hash (* ... *)
+
+let prop_reversible password =
+	decrypt (encrypt password) = password
 
 (* for generating random strings *)
 let arbstring = arbitrary_string
