d46018745a8a533ddefe1f3e52604b55726dfac1
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-10-02 17:25:50 -0400

    decryption fixed passes unit tests

--- ios7crypt.ml
+++ ios7crypt.ml
@@ -81,9 +81,9 @@ let decrypt hash =
 	else
 		try
 			let s = int_of_string (String.sub hash 0 2) in
-			let keys = xlat s (String.length hash - 2) in
-			let p = List.map (int_of_string) (List.map (fun x -> "0x" ^ x) (only_pairs (String.sub hash 2 (String.length hash)))) in
-			let plaintext = List.map2 (lxor) keys p in
+			let keys = xlat s ((String.length hash - 2) / 2) in
+			let ciphertext = List.map (int_of_string) (List.map (fun x -> "0x" ^ x) (only_pairs (String.sub hash 2 (String.length hash - 2)))) in
+			let plaintext = List.map2 (lxor) keys ciphertext in
 			Some (implode (List.map (Char.chr) plaintext))
 		with Failure "int_of_string" -> None
 
