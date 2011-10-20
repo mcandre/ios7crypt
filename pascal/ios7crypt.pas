4e509ee8c546b7a9b5ea34016ef4bc42a6c5f862
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2011-10-20 18:41:13 -0400

    Encrypt working

--- pascal/ios7crypt.pas
+++ pascal/ios7crypt.pas
@@ -3,10 +3,12 @@ program IOS7Crypt;
 {$ELSE}
 unit IOS7Crypt;
 {$ENDIF}
+{$Mode ObjFPC}
 uses
 	sysutils;
 type
 	bytes = array of byte;
+	strings = array of string;
 const
 	XlatSize = 53;
 	XlatPrime : array[0 .. XlatSize - 1] of byte = (
@@ -18,7 +20,6 @@ const
 		$39, $38, $37, $33, $32, $35, $34, $6b,
 		$3b, $66, $67, $38, $37
 	);
-	Empty : bytes = nil;
 {$IFDEF ios7crypt}{$ELSE}
 interface
 function Xlat (i : integer; len : integer) : bytes;
@@ -43,7 +44,7 @@ var
 	Keys : bytes;
 	Ciphertext : bytes;
 	I : integer;
-	Result : string;
+	Hex : string;
 begin
 	if Length(password) = 0 then
 		Encrypt := ''
@@ -64,20 +65,86 @@ begin
 			for I := 0 to Length(password) - 1 do
 				Ciphertext[I] := Keys[I] xor Plaintext[I];
 
-			Result := '';
+			Hex := '';
 
 			for I := 0 to Length(Ciphertext) - 1 do
-				Result := Concat(Result, AnsiLowerCase(Format('%.2x', [Ciphertext[I]])));
+				Hex := Concat(Hex, AnsiLowerCase(Format('%.2x', [Ciphertext[I]])));
 
-			Encrypt := Concat(Format('%.2d', [Seed]), Result);
+			Encrypt := Concat(Format('%.2d', [Seed]), Hex);
 		end;
 end;
 
-function Decrypt (hash : string) : string;
+function OnlyPairs (text : string) : strings;
+var
+	Len : integer;
+	I : integer;
 begin
-	Decrypt := 'abc';
+	Len := Length(text) div 2;
 
-	{ ... }
+	SetLength(OnlyPairs, Len);
+
+	for I := 0 to Len - 1 do
+		OnlyPairs[I] := Copy(text, I * 2 + 1, 2);
+end;
+
+function Decrypt (hash : string) : string;
+var
+	Head : string;
+	Tail : string;
+	Seed : integer;
+	HexPairs : strings;
+	I : integer;
+	Cipherbyte : integer;
+	Ciphertext : bytes;
+	Keys : bytes;
+	Plaintext : bytes;
+	Password : string;
+begin
+	if Length(hash) < 4 then
+		Decrypt := ''
+	else
+		begin
+			Head := Copy(hash, 1, 2);
+			Tail := Copy(hash, 3, Length(hash) - 2);
+
+			try
+				Seed := StrToInt(Head);
+
+				HexPairs := OnlyPairs(Tail);
+
+				for I := 0 to Length(HexPairs) do
+					begin
+						try
+							Cipherbyte := StrToInt(Concat('$', HexPairs[I]));
+
+							SetLength(Ciphertext, I + 1);
+							Ciphertext[I] := Cipherbyte;
+						except
+							on EConvertError do
+							begin
+							end;
+						end;
+					end;
+
+				Keys := Xlat(Seed, Length(Ciphertext));
+
+				SetLength(Plaintext, Length(Ciphertext));
+
+				for I := 0 to Length(Ciphertext) do
+					Plaintext[I] := Keys[I] xor Ciphertext[I];
+
+				SetLength(Password, Length(Plaintext));
+
+				for I := 0 to Length(Plaintext) do
+					{ String indices start at 1, but array indices start at 0 !? }
+					Password[I + 1] := chr(Plaintext[I]);
+
+				Decrypt := Password;
+			except
+				on EConvertError do
+					Decrypt := 'invalid hash';
+			end;
+		end;
 end;
 {$IFDEF ios7crypt}
 var
