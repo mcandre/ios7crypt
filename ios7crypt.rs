1c6af14a97b4524efa454494841892949cc22e34
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-02-08 00:00:24 -0500

    seed working

--- ios7crypt.rs
+++ ios7crypt.rs
@@ -6,7 +6,7 @@
 
 use std;
 
-fn xlat() -> [int] {
+fn xlat_prime() -> [int] {
 	ret [
 		0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f,
 		0x41, 0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72,
@@ -18,10 +18,25 @@ fn xlat() -> [int] {
 	];
 }
 
-fn encrypt(password : str) -> str {
-	let r = std::rand::mk_rng().next_float();
+fn xlat(i : uint, len : uint) -> [int] {
+	let xs : [int] = xlat_prime();
+	let xs_len : uint = vec::len(xs);
 
-	let seed = r * 16.0f;
+	if len < 1u {
+		ret [];
+	}
+	else {
+		let head : int = xs[i % xs_len];
+		let tail : [int] = xlat(i + 1u, len - 1u);
+
+		ret [head] + tail;
+	}
+}
+
+fn encrypt(password : str) -> str {
+	let r : float = std::rand::mk_rng().next_float();
+
+	let seed : int = (r * 16.0f) as int;
 
 	std::io::println("Seed: " + core::int::to_str(seed, 10u));
 
