d1d1e2424fed8dee42a72a14dc1ebb4214f54628
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-02-14 20:53:11 -0500

    gitignore debugging Dart

---
+++ ios7crypt.dart
@@ -0,0 +1,62 @@
+#!/usr/bin/env dart
+
+#library("ios7crypt");
+
+var xlatPrime = [
+	0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f,
+	0x41, 0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72,
+	0x6b, 0x6c, 0x64, 0x4a, 0x4b, 0x44, 0x48, 0x53,
+	0x55, 0x42, 0x73, 0x67, 0x76, 0x63, 0x61, 0x36,
+	0x39, 0x38, 0x33, 0x34, 0x6e, 0x63, 0x78, 0x76,
+	0x39, 0x38, 0x37, 0x33, 0x32, 0x35, 0x34, 0x6b,
+	0x3b, 0x66, 0x67, 0x38, 0x37
+];
+
+xlat(index, len) {
+	if (len < 1) {
+		return [];
+	}
+	else {
+		return [xlatPrime[index % xlatPrime.length]] + xlat(index + 1, len - 1);
+	}
+}
+
+encrypt(password) {
+	if (password.length < 1) {
+		return "";
+	}
+
+	var seed = Math.random() * 16;
+
+	var keys = xlat(seed, password.length);
+
+	var plaintext = []; // ...
+
+	var ciphertext = []; // ...
+
+	var hash = ""; // ...
+
+	return hash;
+}
+
+decrypt(hash) {
+	if (hash.length < 4) {
+		return "";
+	}
+
+	var seed = 0; // ...
+
+	var ciphertext = []; // ...
+
+	var keys = xlat(seed, ciphertext.length);
+
+	var plaintext = []; // ...
+
+	var password = ""; // ...
+
+	return password;
+}
+
+main() {
+	print(encrypt("monkey"));
+}
