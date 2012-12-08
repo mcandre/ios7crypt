457e5eb078e5e6abf2d7edbced332367e269e8af
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-12-08 11:54:17 -0500

    resolving Android rotation issues

--- android/src/main.rb
+++ android/src/main.rb
@@ -11,24 +11,36 @@ class Main
 
 		set_title "IOS7Crypt"
 
+		password = "monkey"
+		hash = "060b002f474b10"
+
+		password = bundle.getString("password", "monkey") unless bundle.nil?
+		hash = bundle.getString("hash", "060b002f474b10") unless bundle.nil?
+
 		self.content_view =
 			linear_layout(:orientation => :vertical) do
 				linear_layout(:orientation => :vertical) do
 					text_view :text => "Password:", :text_size => 24.0
-					@password = edit_text :single_line => true, :text => "monkey",
+
+					@password = edit_text :single_line => true, :text => password,
 						:width => :match_parent, :gravity => :center, :text_size => 24.0
+
 					button :text => "Encrypt", :text_size => 24.0,
 						:on_click_listener => proc { android_encrypt }
 				end
 
 				linear_layout(:orientation => :vertical) do
 					text_view :text => "Hash", :text_size => 24.0
-					@hash = edit_text :single_line => true, :text => "04560408042455",
+
+					@hash = edit_text :single_line => true, :text => hash,
 						:width => :match_parent, :gravity => :center, :text_size => 24.0
+
 					button :text => "Decrypt", :text_size => 24.0,
 						:on_click_listener => proc { android_decrypt }
 				end
 			end
+
+		self.on_save_instance_state(proc { save_state })
 	rescue
 		puts "Exception creating activity: #{$!}"
 		puts $!.backtrace.join("\n")
@@ -37,18 +49,15 @@ class Main
 	private
 
 	def android_encrypt
-		password = @password.text.to_string
-
-		hash = password.encrypt
-
-		@hash.text = hash
+		@hash.text = @password.text.to_string.encrypt
 	end
 
 	def android_decrypt
-		hash = @hash.text.to_string
+		@password.text = @hash.text.to_string.decrypt
+	end
 
-		password = hash.decrypt
-
-		@password.text = password
+	def save_state(bundle)
+		bundle.putString("password", @password.text.to_string)
+		bundle.putString("hash", @hash.text.to_string)
 	end
 end
