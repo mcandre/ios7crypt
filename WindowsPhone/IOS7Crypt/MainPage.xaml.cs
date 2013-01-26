322eb2a61d6172f0ea9311ad2abf33b854226005
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-01-26 16:09:40 -0500

    resolving dependencies

--- WindowsPhone/IOS7Crypt/MainPage.xaml.cs
+++ WindowsPhone/IOS7Crypt/MainPage.xaml.cs
@@ -11,22 +11,32 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 
+using System.Reflection;
+using Microsoft.Scripting;
+using Microsoft.Scripting.Hosting;
+using IronRuby;
+
 namespace IOS7Crypt {
     public partial class MainPage : PhoneApplicationPage {
+        private ScriptEngine engine;
+        private dynamic ios7crypt;
+
         // Constructor
         public MainPage() {
             InitializeComponent();
+            engine = IronRuby.Ruby.CreateRuntime();
+            ios7crypt = engine.RequireFile(@"ios7crypt.rb");
         }
 
         public void Encrypt(object sender, EventArgs e) {
             String password = PasswordBox.Text;
-            String hash = ""; // ...
+            String hash = ios7crypt.encrypt(password);
             HashBox.Text = hash;
         }
 
         public void Decrypt(object sender, EventArgs e) {
             String hash = HashBox.Text;
-            String password = ""; // ...
+            String password = ios7crypt.decrypt(hash);
             PasswordBox.Text = password;
         }
     }
