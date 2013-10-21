fd5616442019fdfd69d700e50ece46ce8e7243ae
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-10-20 23:29:38 -0400

    pylint

--- ios7crypt.py
+++ ios7crypt.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+"""IOS7Crypt password encryptor/decryptor"""
+
 import os, sys
 from getopt import getopt
 
-xlat = [
+XLAT = [
   0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f,
   0x41, 0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72,
   0x6b, 0x6c, 0x64, 0x4a, 0x4b, 0x44, 0x48, 0x53,
@@ -15,12 +17,20 @@ xlat = [
 
 def encrypt(password):
   seed = ord(os.urandom(1)) % 16
-  return "%02d%s" % (seed, "".join(["%02x" % (ord(password[i]) ^ xlat[seed + i]) for i in range(len(password))]))
+  return "%02d%s" % (seed, "".join(
+    ["%02x" % (
+      ord(password[i]) ^ XLAT[seed + i]
+    ) for i in range(len(password))])
+  )
 
 def decrypt(h):
   seed, h = int(h[:2]), h[2:]
-  cipherBytes = [int(h[i : i + 2], 16) for i in range(0, len(h), 2)]
-  return "".join([chr(cipherBytes[i] ^ xlat[(seed + i) % len(xlat)]) for i in range(len(cipherBytes))])
+  cipher_bytes = [int(h[i : i + 2], 16) for i in range(0, len(h), 2)]
+  return "".join(
+    [chr(
+      cipher_bytes[i] ^ XLAT[(seed + i) % len(XLAT)]
+    ) for i in range(len(cipher_bytes))]
+  )
 
 def test():
   # ...
@@ -37,18 +47,22 @@ def usage():
   sys.exit()
 
 def main():
-  ENCRYPT_MODE = "ENCRYPT"
-  DECRYPT_MODE = "DECRYPT"
-  TEST_MODE = "TEST"
+  encrypt_mode = "ENCRYPT"
+  decrypt_mode = "DECRYPT"
+  test_mode = "TEST"
 
-  mode = ENCRYPT_MODE
+  mode = encrypt_mode
   password = ""
   h = ""
 
   optlist, args = [], []
 
   try:
-    optlist, args=getopt(sys.argv[1:], "e:d:th", ["encrypt=", "decrypt=", "test", "help"])
+    optlist, args = getopt(
+      sys.argv[1:],
+      "e:d:th",
+      ["encrypt=", "decrypt=", "test", "help"]
+    )
   except Exception:
     usage()
 
@@ -56,23 +70,23 @@ def main():
     usage()
 
   for option, value in optlist:
-    if option=="-h" or option=="--help":
+    if option == "-h" or option == "--help":
       usage()
-    elif option=="-e" or option=="--encrypt":
-      mode = ENCRYPT_MODE
+    elif option == "-e" or option == "--encrypt":
+      mode = encrypt_mode
       password = value
-    elif option=="-d" or option=="--decrypt":
-      mode=DECRYPT_MODE
+    elif option == "-d" or option == "--decrypt":
+      mode = decrypt_mode
       h = value
-    elif option=="-t" or option=="--test":
-      mode=TEST_MODE
+    elif option == "-t" or option == "--test":
+      mode = test_mode
 
-  if mode == ENCRYPT_MODE:
+  if mode == encrypt_mode:
     print(encrypt(password))
-  elif mode == DECRYPT_MODE:
+  elif mode == decrypt_mode:
     print(decrypt(h))
-  elif mode == TEST_MODE:
+  elif mode == test_mode:
     test()
 
-if __name__=="__main__":
+if __name__ == "__main__":
   main()
