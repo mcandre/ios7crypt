84e37b041321637b90f4a50a53141cc646532499
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-10-23 17:39:13 -0400

    syntax improvements for rust 0.8

--- rust/ios7crypt.rs
+++ rust/ios7crypt.rs
@@ -5,11 +5,11 @@
 #[link(name = "ios7crypt")];
 
 use std;
-import std::getopts::*;
-import result::*;
+use std::getopts::*;
+use result::*;
 
 fn xlat_prime() -> [int] {
-  ret [
+  return [
     0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f,
     0x41, 0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72,
     0x6b, 0x6c, 0x64, 0x4a, 0x4b, 0x44, 0x48, 0x53,
@@ -25,19 +25,19 @@ fn xlat(i : uint, len : uint) -> [int] {
   let xs_len : uint = vec::len(xs);
 
   if len < 1u {
-    ret [];
+    return [];
   }
   else {
     let head : int = xs[i % xs_len];
     let tail : [int] = xlat(i + 1u, len - 1u);
 
-    ret [head] + tail;
+    return [head] + tail;
   }
 }
 
 fn xor(tp : (int, int)) -> int {
   let (a, b) : (int, int) = tp;
-  ret a ^ b;
+  return a ^ b;
 }
 
 fn encrypt(password : str) -> str {
@@ -45,7 +45,7 @@ fn encrypt(password : str) -> str {
 
   let seed : uint = (r * 16.0f) as uint;
 
-  let plaintext : [int] = vec::map(str::chars(password), { |c| ret c as int; });
+  let plaintext : [int] = vec::map(str::chars(password), |c| { return c as int; });
 
   let keys : [int] = xlat(seed, str::char_len(password));
 
@@ -55,11 +55,11 @@ fn encrypt(password : str) -> str {
 
   let ciphertext : [int] = vec::map(zipped, xor);
 
-  ret #fmt(
+  return #fmt(
     "%02d%s",
     seed as int,
     str::connect(
-      vec::map(ciphertext, { |c| ret #fmt("%02x", c as uint); }),
+      vec::map(ciphertext, { |c| return #fmt("%02x", c as uint); }),
       ""
     )
   );
@@ -67,7 +67,7 @@ fn encrypt(password : str) -> str {
 
 fn decrypt(hash : str) -> str {
   if str::char_len(hash) < 2u {
-    ret "";
+    return "";
   }
   else {
     let seedStr : str = str::substr(hash, 0u, 2u);
@@ -78,7 +78,7 @@ fn decrypt(hash : str) -> str {
 
     // ...
 
-    ret "";
+    return "";
   }
 }
 
@@ -111,8 +111,8 @@ fn main(args: [str]) {
   ];
 
   let match = alt getopts(vec::tail(args), opts) {
-    ok(m) { m }
-    err(f) { fail fail_str(f) }
+    Ok(m) { m }
+    Err(f) { fail fail_str(f) }
   };
 
   if opt_present(match, "e") || opt_present(match, "encrypt") {
