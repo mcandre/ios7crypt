e2a5dc50550f7818c3adaa844b68d39186140134
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-10 14:33:02 -0500

    c: using builtin strtol()

--- c/ios7crypt.h
+++ c/ios7crypt.h
@@ -2,7 +2,6 @@ extern int xlat[];
 extern int XLAT_SIZE;
 
 static void usage(char *program);
-static int htoi(char x);
 void encrypt(char *password, char *hash);
 void decrypt(char *hash, char *password);
 bool reversible(void *data);
