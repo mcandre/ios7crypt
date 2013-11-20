d6a9ff7a98efd043626b89c38f95b510cf599a06
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-11-20 16:28:43 -0500

    pep8

--- python/tasks.py
+++ python/tasks.py
@@ -6,6 +6,10 @@ def test():
   run("python ios7crypt.py -d 011e090a500e1f")
 
 @task
+def pep8():
+  run("pep8 .")
+
+@task
 def pylint():
   run("pylint *.py")
 
@@ -13,6 +17,6 @@ def pylint():
 def pyflakes():
   run("pyflakes .")
 
-@task("pylint", "pyflakes")
+@task("pep8", "pylint", "pyflakes")
 def lint():
   pass
