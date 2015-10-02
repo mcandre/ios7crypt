7aa239c5985b10feb3e964d3eb2d7ad45590da17
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-10-02 12:53:30 -0500

    close #9

--- java/src/main/java/us/yellosoft/ios7crypt/CommandLine.java
+++ java/src/main/java/us/yellosoft/ios7crypt/CommandLine.java
@@ -1,93 +1,35 @@
 package us.yellosoft.ios7crypt;
 
-import java.util.ArrayList;
+import java.util.Map;
 
-import gnu.getopt.Getopt;
-import gnu.getopt.LongOpt;
+import org.docopt.Docopt;
 
 /** CLI for IOS7Crypt */
 public final class CommandLine {
   /** Utility class */
   private CommandLine() {}
 
-  /** Print usage information */
-  public static void usage() {
-    System.out.println(
-      "Usage: CommandLine [OPTIONS]\n" +
-      "--help, -h:\n" +
-      "    show help\n" +
-      "--encrypt, -e <password1> <password2> <password3> ...:\n" +
-      "    prints out the encrypted passwords as hashes\n" +
-      "--decrypt, -d <hash1> <hash2> <hash3> ...:\n" +
-      "    prints out the decrypted hashes as passwords"
-    );
-
-    System.exit(0);
-  }
+  /** DocOpt usage spec */
+  public static final String DOC =
+    "Usage:\n" +
+    "  ios7crypt --encrypt=<password>\n" +
+    "  ios7crypt --decrypt=<hash>\n" +
+    "  ios7crypt --help\n" +
+    "Options:\n" +
+    "  -e --encrypt=<password>  Encrypt a password\n" +
+    "  -d --decrypt=<hash>      Decrypt a hash\n" +
+    "  -h --help                Print usage information";
 
   /** Execute CLI
       @param args CLI flags
    */
   public static void main(final String[] args) {
-    String mode = "encrypt";
-    String password = "";
-    String hash = "";
+    Map<String, Object> options = new Docopt(DOC).withVersion("0.0.1").parse(args);
 
-    if (args.length < 1) {
-      usage();
-    }
-
-    StringBuffer valueBuffer = new StringBuffer();
-
-    LongOpt[] longOpts = {
-      new LongOpt("help", LongOpt.NO_ARGUMENT, null, 'h'),
-      new LongOpt("encrypt", LongOpt.REQUIRED_ARGUMENT, null, 'e'),
-      new LongOpt("decrypt", LongOpt.REQUIRED_ARGUMENT, null, 'd')
-    };
-
-    Getopt g = new Getopt("CommandLine", args, "he:d:", longOpts);
-    g.setOpterr(false);
-
-    int c = g.getopt();
-
-    while (c != -1) {
-      switch (c) {
-      case 'e':
-        mode = "encrypt";
-        password = g.getOptarg();
-        break;
-
-      case 'd':
-        mode = "decrypt";
-        hash = g.getOptarg();
-        break;
-
-      default:
-        usage();
-        break;
-      }
-
-      c = g.getopt();
-    }
-
-    ArrayList<String> leftoverArgs = new ArrayList<String>();
-
-    for (int i = g.getOptind(); i < args.length; i++) {
-      leftoverArgs.add(args[i]);
-    }
-
-    if (mode == "encrypt") {
-      System.out.println(IOS7Crypt.encrypt(password));
-
-      for (String arg : leftoverArgs) {
-        System.out.println(IOS7Crypt.encrypt(arg));
-      }
-    } else if (mode == "decrypt") {
-      System.out.println(IOS7Crypt.decrypt(hash));
-
-      for (String arg : leftoverArgs) {
-        System.out.println(IOS7Crypt.decrypt(arg));
-      }
+    if ((String) options.get("--encrypt") != null) {
+      System.out.println(IOS7Crypt.encrypt((String) options.get("--encrypt")));
+    } else if ((String) options.get("--decrypt") != null) {
+      System.out.println(IOS7Crypt.decrypt((String) options.get("--decrypt")));
     }
   }
 }
