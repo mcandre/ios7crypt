0c2929982154eb2ad13b265c84589742c118822c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-02-23 18:26:51 -0600

    cleanup

--- cpp/src/ios7crypt++.cpp
+++ cpp/src/ios7crypt++.cpp
@@ -2,28 +2,24 @@
  * @copyright 2020 YelloSoft
  */
 
+#include <array>
 #include <iostream>
 #include <sstream>
 #include <string>
 
 #include "ios7crypt++/ios7crypt++.hpp"
 
-static std::vector<int> Xlat() {
-    const int bs[53] = {
-        0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f,
-        0x41, 0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72,
-        0x6b, 0x6c, 0x64, 0x4a, 0x4b, 0x44, 0x48, 0x53,
-        0x55, 0x42, 0x73, 0x67, 0x76, 0x63, 0x61, 0x36,
-        0x39, 0x38, 0x33, 0x34, 0x6e, 0x63, 0x78, 0x76,
-        0x39, 0x38, 0x37, 0x33, 0x32, 0x35, 0x34, 0x6b,
-        0x3b, 0x66, 0x67, 0x38, 0x37
-    };
-
-    return std::vector<int>(bs, bs + sizeof(bs)/sizeof(int));
-}
+static const std::array<int, 53> xlat = {
+    0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f,
+    0x41, 0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72,
+    0x6b, 0x6c, 0x64, 0x4a, 0x4b, 0x44, 0x48, 0x53,
+    0x55, 0x42, 0x73, 0x67, 0x76, 0x63, 0x61, 0x36,
+    0x39, 0x38, 0x33, 0x34, 0x6e, 0x63, 0x78, 0x76,
+    0x39, 0x38, 0x37, 0x33, 0x32, 0x35, 0x34, 0x6b,
+    0x3b, 0x66, 0x67, 0x38, 0x37
+};
 
 std::string ios7crypt::Encrypt(uint prng_seed, const std::string &password) {
-    const auto xs = Xlat();
     auto seed = int(rand_r(&prng_seed) % 16);
 
     auto hash = std::stringstream();
@@ -37,7 +33,7 @@ std::string ios7crypt::Encrypt(uint prng_seed, const std::string &password) {
         hash.setf(std::ios::hex, std::ios::basefield);
         hash.width(2);
         hash.fill('0');
-        hash << int(p ^ xs.at(size_t((seed++) % int(xs.size()))));
+        hash << int(p ^ xlat.at(size_t((seed++) % int(xlat.size()))));
     }
 
     return hash.str();
@@ -47,8 +43,6 @@ std::optional<std::string> ios7crypt::Decrypt(const std::string &hash) {
     if (hash.length() < 4 || hash.length() % 2 != 0) {
         return std::nullopt;
     }
-
-    const auto xs = Xlat();
 
     errno = 0;
     auto seed = std::stoi(hash.substr(0, 2), nullptr, 10);
@@ -67,7 +61,7 @@ std::optional<std::string> ios7crypt::Decrypt(const std::string &hash) {
             return std::nullopt;
         }
 
-        password << char(c ^ xs.at(size_t((seed++) % int(xs.size()))));
+        password << char(c ^ xlat.at(size_t((seed++) % int(xlat.size()))));
     }
 
     return password.str();
